8edc0ff8a6b71ed0d16c1ab347cab39eaf22c169
emi-suzuki <emi.clairesuzuki@gmail.com>
2020-06-23 12:29:50 -0400

    Modified pwm node to act like hmc team pwm.

--- racecar_mn/pwm.py
+++ racecar_mn/pwm.py
@@ -1,90 +1,69 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-# node abstraction for using maestro.py driver for servo/motor
+# Abstraction layer for controlling the motors
 
-# standard library imports
-import sys
-import time
-
-# ROS imports
 import rclpy
 from rclpy.parameter import Parameter
 from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy, QoSProfile
-
-from ackermann_msgs.msg import AckermannDriveStamped
-
-
 from . import maestro
+from ackermann_msgs.msg import AckermannDriveStamped
+from std_msgs.msg import String
+import time
 
-CAR_MAX_FORWARD = 0.25 # Parameter('car_max_forward',type_=Parameter.Type.DOUBLE,value=0.25)
-CAR_MAX_BACKWARD = 0.25 # Parameter('car_max_backward',type_=Parameter.Type.DOUBLE,value=0.25)
-CAR_MAX_TURN = float(0.4) # Parameter('car_max_turn', type_=Parameter.Type.DOUBLE, value=0.4)
-STEERING_CENTER_REL_OFFSET = float(0) # Parameter('steering_center_rel_offset', type_=Parameter.Type.DOUBLE, value=0.0)
+PWM_SPEED_MAX = 9000
+PWM_SPEED_MIN = 3000
+PWM_SPEED_CHANNEL = 0
 
-# simple function to do a linear mapping
-def map_val(value, inMin, inMax, outMin, outMax):
-    inSpan = inMax - inMin
-    outSpan = outMax - outMin
-    valScaled = float(value - inMin) / float(inSpan)
-    return outMin + (valScaled * outSpan)
-
+PWM_TURN_LEFT = 9000
+PWM_TURN_RIGHT = 3000
+PWM_TURN_LEFT_CAP = 9000
+PWM_TURN_RIGHT_CAP = 3000
+PWM_TURN_CHANNEL = 1
 
 def main(args=None):
-
-    # default config
-    controller = maestro.Controller()
-
-    # setup motor config
-    controller.setRange(0,3000,9000)
-    controller.setSpeed(0,0)
-    controller.setAccel(0,0)
-    controller.setTarget(0,6000)
-
-    # setup steering config
-    controller.setRange(1,3000,9000)
-    controller.setSpeed(1,0)
-    controller.setAccel(1,0)
-    controller.setTarget(1,6000)
-
-    # msg.drive.speed:
-    # [-1.5, 1.5] -> | -1.5 full reverse | 1.5 full forward |
-    # msg.drive.steering_angle:
-    # [-0.4, 0.4] -> | -0.4 full left    | 0.4 full right   |
-    def motor_callback(msg):
-
-        # print("motor_callback:")
-        # print(f"    >> speed:  {msg.drive.speed}")
-        # print(f"    >> steer:  {msg.drive.steering_angle}")
-
-        if msg.drive.speed >= 0:
-            lin_vel = map_val(msg.drive.speed, 0, CAR_MAX_FORWARD, 6000, 9000)
-        else:
-            lin_vel = map_val(msg.drive.speed, -CAR_MAX_BACKWARD, 0, 3000, 6000)
-
-        turn_angle = map_val(msg.drive.steering_angle, -CAR_MAX_TURN, CAR_MAX_TURN, 8000, 4000)
-
-        # print(f"    << speed:  {lin_vel}")
-        # print(f"    << steer:  {turn_angle}")
-
-        # set drive speed
-        controller.setTarget(0, int(lin_vel))
-
-        # added Y_OFFSET to center servo
-        controller.setTarget(1, int(turn_angle + STEERING_CENTER_REL_OFFSET))
-
-    # init ros
-    rclpy.init(args=args)
+    rclpy.init()
     node = rclpy.create_node('pwm')
-
-    try:
+    
+    try:  
         qos_profile = QoSProfile(depth=1)
         qos_profile.history = QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST
         qos_profile.reliability = QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
         qos_profile.durability = QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE
+        pub = node.create_publisher(String, "/pwm_debug", qos_profile = 1)
+    
+        # initialize servo controller
+        controller = maestro.Controller()
+    
+        def drive_callback(msg):
+            """
+            Callback function passed to the ROS subscriber /drive to compute signals to send to the PWM
+            """
+        
+            # Set drive speed
+            controller.setTarget(PWM_SPEED_CHANNEL, int(msg.drive.speed))
 
-        node.create_subscription(AckermannDriveStamped, '/motor', motor_callback, qos_profile)
+            # Set drive angle
+            controller.setTarget(PWM_TURN_CHANNEL, int(msg.drive.steering_angle))
+        
+            my_msg = String()
+            my_msg.data =  ("Velocity Signal: " + str(msg.drive.speed) + "|" \
+                         "Turn Signal: " + str(msg.drive.steering_angle))
+            pub.publish(my_msg)
 
-        # wait and shutdown
+        node.create_subscription(AckermannDriveStamped, '/drive', drive_callback, qos_profile)
+    
+        # speed config
+        controller.setRange(PWM_SPEED_CHANNEL, PWM_SPEED_MIN, PWM_SPEED_MAX)
+        controller.setSpeed(PWM_SPEED_CHANNEL, 0)
+        controller.setAccel(PWM_SPEED_CHANNEL, 0)
+        controller.setTarget(PWM_SPEED_CHANNEL, (PWM_SPEED_MAX + PWM_SPEED_MIN) // 2)
+
+        # turning config
+        controller.setRange(PWM_TURN_CHANNEL, PWM_TURN_RIGHT, PWM_TURN_LEFT)
+        controller.setSpeed(PWM_TURN_CHANNEL, 0)
+        controller.setAccel(PWM_TURN_CHANNEL, 0)
+        controller.setTarget(PWM_TURN_CHANNEL, (PWM_TURN_RIGHT + PWM_TURN_LEFT) // 2)
+        
         rclpy.spin(node)
 
     except KeyboardInterrupt:
@@ -92,7 +71,5 @@ def main(args=None):
     finally:
         controller.close()
         rclpy.shutdown()
-
-
 if __name__ == '__main__':
     main(sys.argv)
